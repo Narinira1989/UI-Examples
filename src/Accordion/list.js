4f35cc28d70bd6f7251ca0ea18728d375659e4f5
Irina <irinanarin1989@gmail.com>
2023-08-26 22:58:29 +0300

    first commit

---
+++ src/Accordion/list.js
@@ -0,0 +1,14 @@
+export const list = [
+    {
+        title: "Accordion No:1",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio euismod Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio euismod"
+    },
+    {
+        title: "Accordion No:2",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio euismod Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio euismod"
+    },
+    {
+        title: "Accordion No:3",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio euismod Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio euismod"
+    }
+]
