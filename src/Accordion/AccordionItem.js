0a234af574aa409684b1d20ff17c2d6d99a4ed70
Irina <irinanarin1989@gmail.com>
2023-08-27 17:39:30 +0300

    added arrow icon

--- src/Accordion/AccordionItem.js
+++ src/Accordion/AccordionItem.js
@@ -11,7 +11,11 @@ const handleIsOpen = () => {
     return(
         <div className="style">
             <div className={isOpen ? "active" : "default"} onClick={handleIsOpen}>
-                {title}
+                <div className="titleStile">
+                    <h3>{title}</h3>
+                    <img className="arrow" width="48" height="48" src="https://img.icons8.com/sf-regular-filled/48/down-squared.png" alt="down-squared"/>
+                </div>
+
             </div>
             {isOpen && <div className="content">
                 {content}
